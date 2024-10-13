0c65f732e28e7e00742b5a62b061fead1f046c9e
cho-subin <100771469+cho-subin@users.noreply.github.com>
2024-10-13 17:22:49 +0900

    두 값이 정확히 같은 조건 변경

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,9 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
   // 1. 두 값이 정확히 같은지 확인 (참조가 같은 경우)
-  if (typeof objA === typeof objB && objA === objB) {
-    return true;
-  }
+  if (Object.is(objA, objB)) return true;
 
   // 2. 둘 중 하나라도 객체가 아닌 경우 처리
   if (
